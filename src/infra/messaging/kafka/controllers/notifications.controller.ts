c77e84a88bb108866720406236ee11f8868628b1
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2023-01-22 19:25:05 -0300

    criando o controller do kafka para conectar com o servidor

---
+++ src/infra/messaging/kafka/controllers/notifications.controller.ts
@@ -0,0 +1,18 @@
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
+interface SendNotificationPayload {
+  content: string;
+  category: string;
+  recipientId: string;
+}
+
+@Controller()
+export class NotificationsController {
+  @EventPattern('notifications.send-notification')
+  async handleSendNotification(
+    @Payload() { content, category, recipientId }: SendNotificationPayload,
+  ) {
+    console.log(content);
+  }
+}
